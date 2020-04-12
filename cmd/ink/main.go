ab88edbb4280e04f9d0fffe645b248dc3c67ff7a
Nat Welch <nat@natwelch.com>
2020-04-12 22:40:34 +0100

    copy

--- cmd/ink/main.go
+++ cmd/ink/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  	"log"
+	"log"
 	"os"
 
   	"periph.io/x/periph/conn/gpio/gpioreg"
@@ -11,26 +11,30 @@ import (
 )
 
 func main() {
-  if _, err := host.Init(); err != nil {
-	log.Fatal(err)
-}
 
-b, err := spi.Open("SPI0.0")
-if err != nil {
-	log.Fatal(err)
-}
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
 
-dc := gpio.ByName("22")
-reset := gpio.ByName("27")
-busy := gpio.ByName("17")
+	b, err := spireg.Open("SPI0.0")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-dev, err := inky.New(b, dc, reset, busy, &inky.Opts{
-	Model:       inky.PHAT,
-	ModelColor:  inky.Red,
-	BorderColor: inky.Black,
-})
-if err != nil {
-	log.Fatal(err)
-}
+	dc := gpioreg.ByName("22")
+	reset := gpioreg.ByName("27")
+	busy := gpioreg.ByName("17")
 
+	dev, err := inky.New(b, dc, reset, busy, &inky.Opts{
+		Model:       inky.PHAT,
+		ModelColor:  inky.Red,
+		BorderColor: inky.Black,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := dev.Draw(img.Bounds(), img, image.ZP); err != nil {
+		log.Fatal(err)
+	}
 }
